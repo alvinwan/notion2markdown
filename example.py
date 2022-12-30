12d9c0663c5cad5cc247c21bddc9efd309761208
Alvin Wan <alvinwan1@gmail.com>
2022-12-29 17:17:39 -0800

    update example with actual url

--- example.py
+++ example.py
@@ -3,4 +3,4 @@ import os
 
 
 exporter = NotionExporter(token=os.environ["NOTION_TOKEN"])
-exporter.export_url(url=os.environ["NOTION_URL"])
+exporter.export_url(url='https://lvinwan.notion.site/Example-Notion-Page-f8deb4d042034c6c8d03b6de37a99498')
