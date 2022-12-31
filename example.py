ca9bfe8aede82745f3e49ea5c0310f054ec04435
Alvin Wan <alvinwan1@gmail.com>
2022-12-30 18:55:05 -0800

    new, more detailed example, with example token

--- example.py
+++ example.py
@@ -1,7 +1,17 @@
 from notion2markdown import NotionExporter
-import os
 
+# 1. You can create your own Notion integration token by following this tutorial. Just follow step 1:
+# https://developers.notion.com/docs/create-a-notion-integration#step-1-create-an-integration
+token = "secret_wPO2C5mhbFBHeRfFsoS8bxtDBARceyEFUavwMwGDfvi"  # for the demo, use this as-is if you're feeling lazy
 
-exporter = NotionExporter(token=os.environ["NOTION_TOKEN"])
-path = exporter.export_url(url=os.environ["NOTION_URL"])
+# Normally, you'd store this in an environment variable, called NOTION_TOKEN
+# import os
+# token = os.environ["NOTION_TOKEN"]
+
+# 2. Add your integration to a Notion page or database. Just follow step 2:
+# https://developers.notion.com/docs/create-a-notion-integration#step-2-share-a-database-with-your-integration
+url = "https://lvinwan.notion.site/Example-Notion-Page-f8deb4d042034c6c8d03b6de37a99498"  # again, just for the demo, use this as-is if you're feeling lazy
+
+exporter = NotionExporter(token=token)
+path = exporter.export_url(url=url)
 print(f" * Exported to {path}")
